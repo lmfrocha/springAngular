1d22445d3ecb48aceb8e86cd91cc42b14d704cbf
Lucas Marcelino Ferreira Rocha <93248003@vli-logistica.com.br>
2018-12-14 14:52:40 -0200

    Inclusao do metodo PUT para categoria e Pessoa

--- algamoney-api/src/main/java/com/algaworks/alamoneyapi/exceptionhandler/AlgamoneyExceptionHandler.java
+++ algamoney-api/src/main/java/com/algaworks/alamoneyapi/exceptionhandler/AlgamoneyExceptionHandler.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +16,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -35,13 +37,21 @@ public class AlgamoneyExceptionHandler extends ResponseEntityExceptionHandler { 
 		
 	}
 	
+	@ExceptionHandler({EmptyResultDataAccessException.class})
+	public ResponseEntity<Object> handleEmptyResultDataAccessException(EmptyResultDataAccessException ex,WebRequest request) {
+		String mensagemUsuario = messageSource.getMessage("recurso.nao-encontrado", null, LocaleContextHolder.getLocale());
+		String mensagemDev = ex.toString();
+		List<Erro> erros = Arrays.asList(new Erro(mensagemUsuario, mensagemDev));
+		return handleExceptionInternal(ex, erros, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+	}
+	
 	@Override
-		protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
-				HttpHeaders headers, HttpStatus status, WebRequest request) {
-			
-			List<Erro> erros = criarListaDeErros(ex.getBindingResult());
-			return handleExceptionInternal(ex, erros, headers, HttpStatus.BAD_REQUEST, request);
-		}
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+		
+		List<Erro> erros = criarListaDeErros(ex.getBindingResult());
+		return handleExceptionInternal(ex, erros, headers, HttpStatus.BAD_REQUEST, request);
+	}
 	
 	private List<Erro> criarListaDeErros(BindingResult bindingResult){
 		List<Erro> erros = new ArrayList<>();
--- algamoney-api/src/main/java/com/algaworks/alamoneyapi/resource/CategoriaResource.java
+++ algamoney-api/src/main/java/com/algaworks/alamoneyapi/resource/CategoriaResource.java
@@ -5,19 +5,25 @@ import java.util.List;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.alamoneyapi.event.RecursoCriadoEvent;
 import com.algaworks.alamoneyapi.model.Categoria;
+import com.algaworks.alamoneyapi.model.Pessoa;
 import com.algaworks.alamoneyapi.repository.CategoriaRepository;
 
 @RestController //Controlador JSON dispensa uso de notaçoes adicionais
@@ -57,5 +63,23 @@ public class CategoriaResource {
 //		}		
 	}
 	
+	@DeleteMapping("/{codigo}")
+	public void remover(@PathVariable Long codigo){
+		Categoria categoriaExist = categoriaRepository.findOne(codigo);
+		if(categoriaExist == null) {
+			throw new EmptyResultDataAccessException(1);
+		}
+		categoriaRepository.delete(codigo);
+	}
 	
+	@PutMapping("/{codigo}")
+	public ResponseEntity<Categoria> atualiza(@PathVariable Long codigo, Categoria categoria) {
+		Categoria categoriaExist = categoriaRepository.findOne(codigo);
+		if(categoriaExist == null) {
+			throw new EmptyResultDataAccessException(1);
+		}
+		BeanUtils.copyProperties(categoria, categoriaExist, "codigo");
+		categoriaRepository.save(categoriaExist);
+		return ResponseEntity.ok(categoriaExist);
+	}
 }
